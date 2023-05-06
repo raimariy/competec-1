28d8ffe48dc50c952a4b9106faac80653aba00e5
raimariy <132758377+raimariy@users.noreply.github.com>
2023-05-06 14:19:50 +0000

    Aula0

---
+++ turma-A/Aula0.java
@@ -0,0 +1,41 @@
+public class Aula0{
+
+    public static void main(String[] args){
+
+        /*int idade = 16;
+        boolean cafe = false;
+        char[] nome = [{'r', 'a', 'i', 's', 's', 'a'}];
+        System.out.println("Nome " + nome);
+        System.out.println("Idade " + idade);
+        System.out.println("Tomou café: " + cafe);*/
+
+        /*String nome;
+        nome = "Raissa";
+        int idade = 16;
+        System.out.println(nome + ": idade = " + idade);*/
+
+        /*double n1 = 11;
+        double n2 = 2;
+        double resultado = n1 % n2; // % resto
+        System.out.println(resultado);*/
+
+       /* double laranja = 3.50;
+        double uva = 4.90;
+        double soma = laranja + uva;
+       
+        //uva += laranja;
+        //System.out.println("Valor da uva: " + uva + "\n");
+
+        System.out.println("Valor da laranja: " + laranja + "\n" + "Valor da uva: " + uva + "\n" + "Resultado: " + soma);*/  
+
+        /*String nome1 = "Cristiano Ronaldo";
+        String nome2 = "Manoel";
+        System.out.printf("%s e %s", nome1, nome2);
+
+        int n1 = 2;
+        int n2 = 1;
+        System.out.printf("%d e %d", n1, n2);*/
+
+
+    }
+}
