01ae6712ff2861b280b3c9205803c5840b189f16
raimariy <132758377+raimariy@users.noreply.github.com>
2023-08-12 13:27:34 +0000

    Aula4

---
+++ turma-A/Aula4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Aula4 {
+
+    public static void main(String[] args){
+
+        Scanner ET = new Scanner(System.in);
+
+        //imprimir tabela
+        int[][] tabela = {{4,6}, {1,7}};
+        
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < 2; j++){
+            System.out.print(tabela[i][j] + " ");
+            }
+        }
+    }
+}
